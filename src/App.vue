86e756ebafc71868332622c60b2cede626d9ffda
javierpb275 <javierpb275@gmail.com>
2021-11-09 17:18:24 +0100

    created header component and added to App.vue.
    fixed problem with: Expected linebreaks to be 'LF' but found 'CRLF'. Added linebreak-style error rule for windows

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,6 @@
 <template>
   <div>
-    <!-- Header -->
-    <header id="header" class="bg-gray-700">
-      <nav class="container mx-auto flex justify-start items-center py-5 px-4">
-        <!-- App Name -->
-        <a class="text-white font-bold uppercase text-2xl mr-4" href="#"
-          >Music</a
-        >
-
-        <div class="flex flex-grow items-center">
-          <!-- Primary Navigation -->
-          <ul class="flex flex-row mt-1">
-            <!-- Navigation Links -->
-            <li>
-              <a class="px-2 text-white" href="#">Login / Register</a>
-            </li>
-            <li>
-              <a class="px-2 text-white" href="#">Manage</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-
+    <app-header />
     <!-- Introduction -->
     <section class="mb-8 py-20 text-white text-center relative">
       <div
@@ -787,3 +765,14 @@
     </div>
   </div>
 </template>
+
+<script>
+import AppHeader from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+  },
+};
+</script>
