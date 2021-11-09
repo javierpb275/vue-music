ce013bb2315d7dc5eab60c30edb0a1339e073476
javierpb275 <javierpb275@gmail.com>
2021-11-09 17:43:21 +0100

    created auth component and added to App.vue. fixed problem with linebreak-style again but using unix instead of windows

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
-    'linebreak-style': ["error", "windows"],
+    'linebreak-style': ["error", "unix"],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
