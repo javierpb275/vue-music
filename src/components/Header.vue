ab298210c4d15b250de334fc99823a1c776d7b4b
javierpb275 <javierpb275@gmail.com>
2021-11-09 18:19:13 +0100

    using mapMutations to access the mutations created in the header component.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,12 +24,15 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
 export default {
   name: 'Header',
   methods: {
-    toggleAuthModal() {
+    ...mapMutations(['toggleAuthModal']),
+    /* toggleAuthModal() {
       this.$store.commit('toggleAuthModal');
-    },
+    }, */
   },
 };
 </script>
