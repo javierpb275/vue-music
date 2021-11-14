9150cf956d43bd05432c97c21f61166d2be084f5
javierpb275 <javierpb275@gmail.com>
2021-11-14 13:01:37 +0100

    added rules to email field

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -170,8 +170,9 @@
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input
+              <vee-field
                 type="email"
+                name="email"
                 class="
                   block
                   w-full
@@ -186,6 +187,7 @@
                 "
                 placeholder="Enter Email"
               />
+              <ErrorMessage class="text-red-600" name="email"/>
             </div>
             <!-- Age -->
             <div class="mb-3">
@@ -309,7 +311,7 @@ export default {
       tab: "login",
       schema: {
         name: "required|min:3|max:100|alpha_spaces",
-        email: "",
+        email: "required|min:3|max:100|email",
         age: "",
         password: "",
         confirm_password: "",
