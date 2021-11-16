60e89adecda5d8ea8671662d8e1f086c4feef5ba
javierpb275 <javierpb275@gmail.com>
2021-11-16 17:35:19 +0100

    customized error messages and added and changed some rules

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -327,9 +327,9 @@ export default {
         email: "required|min:3|max:100|email",
         age: "required|min_value:18|max_value:100",
         password: "required|min:3|max:100",
-        confirm_password: "confirmed:@password",
-        country: "required|excluded:Antarctica",
-        tos: "required",
+        confirm_password: "passwords_mismatch:@password",
+        country: "required|country_excluded:Antarctica",
+        tos: "tos",
       },
       userData: {
         country: 'USA'
