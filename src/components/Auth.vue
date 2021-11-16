b653c7b87f17274b5efc4702c44791c6c7fcda3d
javierpb275 <javierpb275@gmail.com>
2021-11-16 18:25:10 +0100

    created data properties and used them to show message when function register is executed when form is submitted

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -144,6 +144,10 @@
             </button>
           </form>
           <!-- Registration Form -->
+          <div class="text-white text-center font-bold p-5 mb-4" 
+          v-if="reg_show_alert" :class="reg_alert_variant">
+            {{reg_alert_msg}}
+          </div>
           <vee-form v-show="tab === 'register'" :validation-schema="schema"
           @submit="register" :initial-values="userData">
             <!-- Name -->
@@ -291,8 +295,7 @@
               <label class="inline-block">Accept terms of service</label>
             </div>
             <ErrorMessage class="text-red-600" name="tos"/>
-            <button
-              type="submit"
+            <button type="submit" :disabled="reg_in_submission"
               class="
                 block
                 w-full
@@ -333,7 +336,11 @@ export default {
       },
       userData: {
         country: 'USA'
-      }
+      },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: 'bg-blue-500',
+      reg_alert_msg: 'Please wait! Your account is being created'
     };
   },
   computed: {
@@ -344,8 +351,19 @@ export default {
   },
   methods: {
     ...mapMutations(["toggleAuthModal"]),
+    
     register(values) {
-      console.log(values)
+
+      this.reg_show_alert=true;
+      this.reg_in_submission=true;
+      this.reg_alert_variant='bg-blue-500.';
+      this.reg_alert_msg= 'Please wait! Your account is being created.';
+
+      this.reg_alert_variant = 'bg-green-500';
+      this.reg_alert_msg= 'Success! Your account has been created.';
+
+      console.log(values);
+
     }
   },
 };
