27fca5616a9ef062e0d32b9e05a977572c904c15
javierpb275 <javierpb275@gmail.com>
2021-11-17 19:12:02 +0100

    installed firebase and added configuration to connect to our firebase app

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import VeeValidatePlugin from './plugins/validation';
+import './plugins/firebase';
 import './assets/tailwind.css';
 import './assets/main.css';
 
---
+++ src/plugins/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+export default initializeApp(firebaseConfig);
