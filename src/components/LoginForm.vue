2930ab031b966bf41c43be3856e449aa5ccfdd6c
javierpb275 <javierpb275@gmail.com>
2021-11-17 17:55:33 +0100

    disabled login button when user is loggin in and showing message when it is done

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <!-- Login Form -->
-    <vee-form
-      :validation-schema="loginSchema"
-      @submit="login"
+    <div
+      class="text-white text-center font-bold p-5 mb-4"
+      v-if="login_show_alert"
+      :class="login_alert_variant"
     >
+      {{ login_alert_msg }}
+    </div>
+    <vee-form :validation-schema="loginSchema" @submit="login">
       <!-- Email -->
       <div class="mb-3">
         <label class="inline-block mb-2">Email</label>
@@ -51,6 +55,7 @@
       </div>
       <button
         type="submit"
+        :disabled="login_in_submission"
         class="
           block
           w-full
@@ -78,10 +83,22 @@ export default {
         email: "required|email",
         password: "required|min:3|max:32",
       },
+      login_in_submission: false,
+      login_show_alert: false,
+      login_alert_variant: "bg-blue-500",
+      login_alert_msg: "Please wait! We are logging you in.",
     };
   },
   methods: {
     login(values) {
+      this.login_in_submission = true;
+      this.login_show_alert = true;
+      this.login_alert_variant = "bg-blue-500.";
+      this.login_alert_msg = "Please wait! Your account is being created.";
+
+      this.login_alert_variant = "bg-green-500";
+      this.login_alert_msg = "Success! You are now logged in.";
+
       console.log(values);
     },
   },
--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -210,12 +210,14 @@ export default {
   },
   methods: {
     register(values) {
-      this.reg_show_alert = true;
       this.reg_in_submission = true;
+      this.reg_show_alert = true;
       this.reg_alert_variant = "bg-blue-500.";
       this.reg_alert_msg = "Please wait! Your account is being created.";
+
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Success! Your account has been created.";
+
       console.log(values);
     },
   },
