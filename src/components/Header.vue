04f3e5e87540e9eea60a792cc8c0faa9ea384ec7
javierpb275 <javierpb275@gmail.com>
2021-11-20 17:31:01 +0100

    added global style for the router-links. remove those styles only for the title of the app.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
       <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/"
-        >Music</router-link
+        exact-active-class="no-active">Music</router-link
       >
 
       <div class="flex flex-grow items-center">
