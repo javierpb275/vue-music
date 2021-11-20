d25ab1f5922fae47c7df4e5412a6473d01aeb96b
javierpb275 <javierpb275@gmail.com>
2021-11-20 17:36:46 +0100

    added name property to the routes and use name instead of the path in the router-links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/"
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" :to="{name: 'home'}"
         exact-active-class="no-active">Music</router-link
       >
 
@@ -12,7 +12,7 @@
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li>
-            <router-link class="px-2 text-white" to="/about">About</router-link>
+            <router-link class="px-2 text-white" :to="{name: 'about'}">About</router-link>
           </li>
           <li v-if="!userLoggedIn">
             <a
@@ -25,7 +25,7 @@
           </li>
           <template v-else>
             <li>
-              <router-link class="px-2 text-white" to="/manage">Manage</router-link>
+              <router-link class="px-2 text-white" :to="{name: 'manage'}">Manage</router-link>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="signout"
