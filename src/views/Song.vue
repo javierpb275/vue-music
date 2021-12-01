0bf6be09333c476c3c8ac5302c6158ab8af23a17
javierpb275 <javierpb275@gmail.com>
2021-12-01 17:20:56 +0100

    allowing authorized users to add comments to the songs.

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -41,8 +41,17 @@
           <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
         </div>
         <div class="p-6">
-          <form>
-            <textarea
+          <div
+            class="text-white text-center font-bold p-4 mb-4"
+            v-if="comment_show_alert"
+            :class="comment_alert_variant"
+          >
+            {{ comment_alert_message }}
+          </div>
+          <vee-form :validation-schema="schema" @submit="addComment" v-if="userLoggedIn">
+            <vee-field
+              as="textarea"
+              name="comment"
               class="
                 block
                 w-full
@@ -57,14 +66,16 @@
                 mb-4
               "
               placeholder="Your comment here..."
-            ></textarea>
+            ></vee-field>
+            <ErrorMessage class="text-red-600" name="comment" />
             <button
               type="submit"
               class="py-1.5 px-3 rounded text-white bg-green-600 block"
+              :disabled="comment_in_submission"
             >
               Submit
             </button>
-          </form>
+          </vee-form>
           <!-- Sort Comments -->
           <select
             class="
@@ -165,14 +176,25 @@
 </template>
 
 <script>
-import { songsCollection } from "../plugins/firebase";
+import { songsCollection, auth, commentsCollection } from "../plugins/firebase";
+import { mapState } from 'vuex';
 
 export default {
   name: "Song",
   data() {
     return {
       song: {},
+      schema: {
+        comment: "required|min:3",
+      },
+      comment_in_submission: false,
+      comment_show_alert: false,
+      comment_alert_variant: "bg-blue-500",
+      comment_alert_message: "Please wait! Your comment is being submitted",
     };
+  },
+  computed: {
+    ...mapState(['userLoggedIn'])
   },
   async created() {
     const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
@@ -182,6 +204,31 @@ export default {
       return;
     }
     this.song = docSnapshot.data();
+  },
+  methods: {
+    async addComment(values, { resetForm }) {
+      this.comment_in_submission = true;
+      this.comment_show_alert = true;
+      this.comment_alert_variant = "bg-blue-500";
+      this.comment_alert_message =
+        "Please wait! Your comment is being submitted";
+
+      const comment = {
+        content: values.comment,
+        datePosted: new Date().toString(),
+        sid: this.$route.params.id,
+        name: auth.currentUser.displayName,
+        uid: auth.currentUser.uid,
+      };
+
+      await commentsCollection.add(comment);
+
+      this.comment_in_submission = false;
+      this.comment_alert_variant = "bg-green-500";
+      this.comment_alert_message = "Comments added!";
+
+      resetForm();
+    },
   },
 };
 </script>
