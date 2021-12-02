83c255e21e0a48b767d3a5512237f226ec35d912
javierpb275 <javierpb275@gmail.com>
2021-12-02 16:56:19 +0100

    updating number of comments of a song when added new comment and showing it in the template.

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -37,7 +37,7 @@
       >
         <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
           <!-- Comment Count -->
-          <span class="card-title">Comments (15)</span>
+          <span class="card-title">Comments ({{ song.comment_count }})</span>
           <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
         </div>
         <div class="p-6">
@@ -184,6 +184,12 @@ export default {
       };
 
       await commentsCollection.add(comment);
+
+      this.song.comment_count += 1;
+
+      await songsCollection.doc(this.$route.params.id).update({
+        comment_count: this.song.comment_count,
+      });
 
       this.getComments();
 
